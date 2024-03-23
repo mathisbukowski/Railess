d4281cdc0ab9375829e6b41bebae23e3bc34813f
Detoeuf Alexandre <alexandre.detoeuf@epitech.eu>
2024-03-23 17:25:52 +0100

    [+] Add exemple in about.jsx with request to API with react and SWR

--- server/api/api.js
+++ server/api/api.js
@@ -4,7 +4,7 @@ import { get_stop_id_by_name } from "../sncf-utils/gtfs_functions.js";
 const apiRoute = express.Router();
 
 apiRoute.get('/getStopIdByName/:name', (req, res) => {
-    res.json(get_stop_id_by_name(req.params.name));
+    res.json({id: get_stop_id_by_name(req.params.name)});
 });
 
 export { apiRoute };
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useSWR from 'swr'
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const About = () => {
+    const { data, error, isLoading } = useSWR('/api/getStopIdByName/Libercourt', fetcher)
+
+    if (error) return <div>Failed to fetch API.</div>
+    if (isLoading) return <div>Loading...</div>
     return (
         <div>
             <h1>About Page</h1>
+            <p>{data.id}</p>
         </div>
     );
 };
