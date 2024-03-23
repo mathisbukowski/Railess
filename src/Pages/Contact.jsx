20e5fbaab43c23f50aac1b491b74924290bd4048
Alex <alexis.salaun@epitech.eu>
2024-03-23 19:28:59 +0100

    [~] Contact -> add info contact

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import Navbar from "../Components/Navbar.jsx";
 import Newsletter from "../Components/Newsletter.jsx";
 import Footer from "../Components/Footer.jsx";
-import Navbar from "../Components/Navbar.jsx";
 
 const Contact = () => {
     return (
         <div>
             <Navbar />
             <div className="w-full py-[5rem] px-4 bg-white">
-                <h1 style={{ textAlign: 'center', fontWeight: 'bold', color: 'black', margin: 'auto' }} class = 'text-4xl'>Contact Us</h1>
-
+                <div className="container mx-auto py-10">
+                    <h1 className="text-4xl font-bold text-center text-black-800 mb-8">Contact Us</h1>
+                    <div className="flex justify-center">
+                        <div className="max-w-lg w-full">
+                            <p className="text-lg text-black-600 mb-4">We'd love to hear from you. Contact us using the form below:</p>
+                        </div>
+                    </div>
+                    <div className="mt-8 text-center">
+                        <p className="text-lg text-black-600">You can also reach us via:</p>
+                        <p className="text-lg text-black-600">Email: railess@gmail.com</p>
+                    </div>
+                </div>
             </div>
             <Newsletter />
             <Footer />
