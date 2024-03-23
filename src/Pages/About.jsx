8858ec58b75bbdb4b1e3837b57f3c705e0fdd414
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-03-23 20:16:36 +0100

    [+] add the about page

---
+++ src/Pages/About.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../Components/Navbar.jsx";
+import Footer from "../Components/Footer.jsx";
+import Newsletter from "../Components/Newsletter.jsx";
+import AboutComponent from "../Components/About.jsx";
+
+const About = () => {
+    return (
+        <>
+            <Navbar />
+            <AboutComponent />
+            <Footer />
+        </>
+    )
+}
+
+export default About;
