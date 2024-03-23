350a0979343b759dec302c29fee6c0a543e34c5e
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-03-23 10:52:13 +0100

    [+] add the HomePage*

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,22 @@
+import Navbar from "../Components/Navbar.jsx";
+import Hero from "../Components/Hero.jsx";
+import Cards from "../Components/Cards.jsx";
+import Newsletter from "../Components/Newsletter.jsx";
+import Footer from "../Components/Footer.jsx";
+
+
+function Home() {
+
+    return (
+        <>
+            <Navbar />
+            <Hero />
+            <Cards />
+            <Newsletter />
+            <Footer />
+        </>
+
+    )
+}
+
+export default Home;
