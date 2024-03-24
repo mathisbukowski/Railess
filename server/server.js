46abd08ccd381f16bb9027ed23e7d756e734222f
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-03-24 16:46:07 +0100

    [~] edit port

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import { apiRoute } from './api/api.js';
 
 // Constants
 const isProduction = process.env.NODE_ENV === 'production'
-const port = process.env.PORT || 5173
+const port = process.env.PORT || 3000
 const base = process.env.BASE || '/'
 
 // Cached production assets
