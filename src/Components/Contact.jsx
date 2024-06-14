05954c6aadbdd7bbefdd77b794e9a8ccd528bd5f
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-06-14 15:13:24 +0200

    fix(contact): fix gramatical errors

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -44,7 +44,7 @@ export default function ContactComponent() {
                         type="submit"
                         className="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-700"
                     >
-                        Envoyer
+                        Send
                     </button>
                 </form>
             </div>
